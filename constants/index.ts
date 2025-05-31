bb6ce7f97b2f73ae49c2f915e7aceb2b3504febb
Unathi Mathokazi <unathimathokaziu@gmail.com>
2025-05-31 12:39:39 +0200

    Cleaned up layout and fixed build error

---
+++ constants/index.ts
@@ -0,0 +1,22 @@
+import { PropertyProps } from '../interfaces';
+
+export const PROPERTYLISTINGSAMPLE: PropertyProps[] = [
+  {
+    name: "Villa Ocean Breeze",
+    address: {
+      state: "Seminyak",
+      city: "Bali",
+      country: "Indonesia"
+    },
+    rating: 4.89,
+    category: ["Luxury Villa", "Pool", "Free Parking"],
+    price: 3200,
+    offers: {
+      bed: "3",
+      shower: "3",
+      occupants: "4-6"
+    },
+    image: "https://example.com/image1.jpg",
+    discount: ""
+  }
+];
